11eea07a24c36539f5bf654944d87425cb8d07b3
Alexander St. John <avstjohn@icloud.com>
2019-06-12 22:07:41 -0500

    sh

--- run.sh
+++
@@ -1,2 +0,0 @@
-cd "C:/Users/Communityscience/Desktop/ShapefilesCopy"
-python "C:/Users/Communityscience/Desktop/ShapefilesCopy" "2930" "6099" "NAD_1927_To_NAD_1983_NADCON + NAD_1983_To_HARN_Wisconsin"
