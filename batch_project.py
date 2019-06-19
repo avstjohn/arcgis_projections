def0925758dbcb3932207bf24584693d77e9a832
Alexander St. John <avstjohn@icloud.com>
2019-06-19 14:19:44 -0500

    added logfile, os.remove, os.rename

--- batch_project.py
+++ batch_project.py
@@ -8,7 +8,8 @@ cwd = os.getcwd()
 arcpy.env.workspace = cwd
 arcpy.env.overwriteOutput = True
 
-output_dir = cwd + "/Reprojected"
+logfile = open(cwd, 'w+')
+logfile.write("Beginning reprojection of directory " + cwd + " and subdirectories.", "\n")
 
 # coordinate system (spatial reference ID) to Project to (e.g., 2930)
 cs_Project = int(sys.argv[1])
@@ -25,10 +26,11 @@ output_cs_Define = arcpy.SpatialReference(cs_Define)
 # Walk through directories and grab files to be projected
 input_files = []
 for root, dirs, files in os.walk(cwd):
-	for name in files:
-		extension = name.split(".")[-1]
-		if (extension == "shp" or extension == "gdb"):
-			input_files.append(root + "/" + name)
+	for filename in files:
+		extension = filename.split(".")[-1]
+		# if (extension == "shp" or extension == "gdb"):
+		if (extension == "shp"):
+			input_files.append(root + "/" + filename)
 
 # Run through list of files, project, and define
 for file in input_files:
@@ -36,44 +38,43 @@ for file in input_files:
 	sr_name = spatial_ref.PCSName
 	sr_code = spatial_ref.PCSCode
 
-	fc_name = file.split("/")[-1]
+	# fc_name = file.split("/")[-1]
+	output_dataset = file.split(".")[0] + "_Reprojected." + file.split(".")[-1]
 
 	# Project to SR 2930
+	logfile.write("Running Project to PCSCode " + str(output_cs_Project) + " for " + file, "\n")
 	if sr_code == cs_Project or sr_code == cs_Define:
-		print(file + " already in " + sr_name + ", Code = " + str(sr_code), "\n", "Skipping.", "\n")
+		logfile.write(file + " already in " + sr_name + ", Code = " + str(sr_code), "\n", "Skipping.", "\n")
 		continue
 	elif sr_name == "Unknown":
-		print("Unknown SR for " + file, "\n")
+		logfile.write("Unknown SR for " + file, "\n")
 		input_cs = output_cs_Project
 		arcpy.Project_management(in_dataset=file,
                                 in_coor_system=input_cs,
-								out_dataset=output_dir + "/" + fc_name,
+								out_dataset=output_dataset,
 								out_coor_system=output_cs_Project)
 	elif sr_name == "NAD_1927_StatePlane_Wisconsin_South_FIPS_4803":
-		print("Known SR for " + file + ": " + sr_name + ", Code = " + str(sr_code), "\n")
+		logfile.write("Known SR for " + file + ": " + sr_name + ", Code = " + str(sr_code), "\n")
 		arcpy.Project_management(in_dataset=file,
-								out_dataset=output_dir + "/" + fc_name,
+								out_dataset=output_dataset,
 								out_coor_system=output_cs_Project,
 								transform_method=gcs_transformation)
 	else: # Catches any other spatial references
-		print("Known SR for " + file + ": " + sr_name + ", Code = " + str(sr_code), "\n")
+		logfile.write("Known SR for " + file + ": " + sr_name + ", Code = " + str(sr_code), "\n")
 		arcpy.Project_management(in_dataset=file,
-								out_dataset=output_dir + "/" + fc_name,
+								out_dataset=output_dataset,
 								out_coor_system=output_cs_Project)
+	logfile.write("\n")
 
 	# Define Projection from SR 2930 -> 6609
 	if sr_code == cs_Define:
 		continue
 	else: # Catches any other spatial references
-		arcpy.DefineProjection_management(in_dataset=output_dir + "/" + fc_name,
+		logfile.write("Running Define Projection to PCSCode" + str(output_cs_Define) + " for " + output_dataset, "\n")
+		arcpy.DefineProjection_management(in_dataset=output_dataset,
 										coor_system=output_cs_Define)
 
-# Toy example 
-# Better way is probably to write layer to same directory with "_reprojected" appended
-# Go through and delete old layers
-# Remove "_reprojected" from name
+	os.remove(file)
+	os.rename(output_dataset, file)
 
-# Copy reprojected layers from output directory back to input directory
-source = output_dir
-destination = cwd + "/copy"
-copy_tree(source, destination)
+logfile.close()
